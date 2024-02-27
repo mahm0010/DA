165ab313167c2517c41d296f5bdaf2e932aae87a
mahm0010 <mahm0010@red.ujaen.es>
2024-02-27 13:20:16 +0100

    pruebas casi

--- BusqTernaria/main.c
+++ BusqTernaria/main.c
@@ -22,7 +22,7 @@ int busquedaClasica(ivector v, int inf, int sup, int valorBuscado) {
 
 int busqTernaria(ivector v, int inf, int sup, int valorBuscado) {
     int tam = sup - inf;
-    imprimeVector(v, tam+1);
+    //imprimeVector(v, tam+1);
     int posicion = -1;
 
     if (tam <= umbral) {
@@ -32,10 +32,13 @@ int busqTernaria(ivector v, int inf, int sup, int valorBuscado) {
         int tercio2 = inf + (2*tam / 3);
 
         if (v[inf] >= valorBuscado) {
+            printf("[BUSQUEDATERNARIA] Buscando en inf=%d | sup=%d\n", inf, tercio-1);
             posicion = busqTernaria(v, inf, tercio-1, valorBuscado);
         }else if (v[tercio] >= valorBuscado) {
+            printf("[BUSQUEDATERNARIA] Buscando en inf=%d | sup=%d\n", tercio, tercio2-1);
             posicion = busqTernaria(v, tercio, tercio2-1, valorBuscado);
         }else{
+            printf("[BUSQUEDATERNARIA] Buscando en inf=%d | sup=%d\n", tercio2, sup);
             posicion = busqTernaria(v, tercio2, sup, valorBuscado);
         }
     }
@@ -48,15 +51,14 @@ int main() {
     int tam = 6;
     ivector v = icreavector(tam);
     v[0] = 1;
-    v[1] = 3;
-    v[2] = 88;
-    v[3] = 1;
-    v[4] = 4;
-    v[5] = 2;
-    v[6] = 42;
+    v[1] = 2;
+    v[2] = 3;
+    v[3] = 4;
+    v[4] = 5;
+    v[5] = 6;
     imprimeVector(v, tam);
-    int valorBuscado = 2;
-    printf("El valor buscado esta en la posicion %d\n", busqTernaria(v, 0, tam-1, valorBuscado));
+    int valorBuscado = 4;
+    printf("El valor buscado (%d) esta en la posicion %d\n", valorBuscado, busqTernaria(v, 0, tam-1, valorBuscado));
 
     return 0;
 }
