0bab50a7076ccc5a0bbb2a1acefe1f8b9585f791
mahm0010 <mahm0010@red.ujaen.es>
2024-02-28 13:08:22 +0100

    busqTernaria funcionando

---
+++ BusqTernaria/cmake-build-debug/CMakeFiles/BusqTernaria.dir/ivector.c.obj.d
@@ -0,0 +1,24 @@
+CMakeFiles/BusqTernaria.dir/ivector.c.obj: \
+ D:\Todo\ lo\ mio\Universidad_Local\DA\BusqTernaria\ivector.c \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/stdlib.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/limits.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/syslimits.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/limits.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/malloc.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/mm_malloc.h \
+ C:/Users/alemi/AppData/Local/JetBrains/Toolbox/apps/CLion-Nova/ch-0/241.11109/bin/mingw/x86_64-w64-mingw32/include/errno.h \
+ D:\Todo\ lo\ mio\Universidad_Local\DA\BusqTernaria\ivector.h
--- BusqTernaria/main.c
+++ BusqTernaria/main.c
@@ -22,7 +22,6 @@ int busquedaClasica(ivector v, int inf, int sup, int valorBuscado) {
 
 int busqTernaria(ivector v, int inf, int sup, int valorBuscado) {
     int tam = sup - inf;
-    //imprimeVector(v, tam+1);
     int posicion = -1;
 
     if (tam <= umbral) {
@@ -31,10 +30,11 @@ int busqTernaria(ivector v, int inf, int sup, int valorBuscado) {
         int tercio = (inf + tam) / 3;
         int tercio2 = inf + (2*tam / 3);
 
-        if (v[inf] >= valorBuscado) {
+
+        if (v[inf] <= valorBuscado && v[tercio] > valorBuscado) {
             printf("[BUSQUEDATERNARIA] Buscando en inf=%d | sup=%d\n", inf, tercio-1);
             posicion = busqTernaria(v, inf, tercio-1, valorBuscado);
-        }else if (v[tercio] >= valorBuscado) {
+        }else if (v[tercio] <= valorBuscado && v[tercio2] > valorBuscado) {
             printf("[BUSQUEDATERNARIA] Buscando en inf=%d | sup=%d\n", tercio, tercio2-1);
             posicion = busqTernaria(v, tercio, tercio2-1, valorBuscado);
         }else{
@@ -57,7 +57,7 @@ int main() {
     v[4] = 5;
     v[5] = 6;
     imprimeVector(v, tam);
-    int valorBuscado = 4;
+    int valorBuscado = 6;
     printf("El valor buscado (%d) esta en la posicion %d\n", valorBuscado, busqTernaria(v, 0, tam-1, valorBuscado));
 
     return 0;
